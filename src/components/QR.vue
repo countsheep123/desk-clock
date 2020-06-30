847d2e9393cc41422e86ab481264457010e4d93c
countsheep123 <countsheep123@mbp.local>
2020-07-01 00:03:08 +0900

    add qr code config

---
+++ src/components/QR.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="component">
+    <vue-qrcode :value="val" :options="option"></vue-qrcode>
+  </div>
+</template>
+
+<script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+
+export default {
+  components: {
+    VueQrcode
+  },
+  props: {
+    value: String
+  },
+  data() {
+    return {
+      val: this.value,
+      option: {
+        errorCorrectionLevel: "H",
+        maskPattern: 0,
+        margin: 2,
+        scale: 1,
+        width: 200,
+        color: {
+          dark: "#000000FF",
+          light: "#FFFFFFFF"
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.component {
+  margin: 0.5rem;
+}
+</style>
--- src/views/Config.vue
+++ src/views/Config.vue
@@ -1,35 +1,92 @@
 <template>
   <div class="component">
-    <Config
-      config-key="open_weather"
-      :config-value="openWeatherApiKey"
-      placeholder="Enter your Open Weather API key"
-    />
-    <Config
-      config-key="nature_remo"
-      :config-value="natureRemoAccessToken"
-      placeholder="Enter your Nature Remo access token"
-    />
+    <div v-if="isQRShown">
+      <button v-on:click="closeQR">Close</button>
+      <QR :value="qrValue" />
+    </div>
+    <div v-else>
+      <Config
+        config-key="open_weather"
+        :config-value="openWeatherApiKey"
+        placeholder="Enter your Open Weather API key"
+      />
+      <Config
+        config-key="nature_remo"
+        :config-value="natureRemoAccessToken"
+        placeholder="Enter your Nature Remo access token"
+      />
+      <button v-on:click="showQR">Show QR</button>
+    </div>
   </div>
 </template>
 
 <script>
 import Config from "@/components/Config.vue";
+import QR from "@/components/QR.vue";
+import CryptoJS from "crypto-js";
 
 export default {
   components: {
-    Config
+    Config,
+    QR
   },
   data() {
     return {
+      isQRShown: false,
+      qrValue: "",
       openWeatherApiKey: "",
-      natureRemoAccessToken: ""
+      natureRemoAccessToken: "",
+      secret: "desk-clock"
     };
   },
   created: function() {
     const config = this.$store.state.config;
+
+    if (this.$route.query["open_weather"]) {
+      config["open_weather"] = this.decrypt(
+        decodeURIComponent(this.$route.query["open_weather"])
+      );
+    }
+    if (this.$route.query["nature_remo"]) {
+      config["nature_remo"] = this.decrypt(
+        decodeURIComponent(this.$route.query["nature_remo"])
+      );
+    }
+    this.$store.commit("setConfig", config);
+
     this.openWeatherApiKey = config["open_weather"];
     this.natureRemoAccessToken = config["nature_remo"];
+
+    this.$router.push(this.$route.path).catch(() => {});
+  },
+  methods: {
+    showQR: function() {
+      this.isQRShown = true;
+      const config = this.$store.state.config;
+      this.qrValue =
+        this.getHostPort() +
+        "/desk-clock/#/config" +
+        "?open_weather=" +
+        encodeURIComponent(this.encrypt(config["open_weather"])) +
+        "&nature_remo=" +
+        encodeURIComponent(this.encrypt(config["nature_remo"]));
+      console.log(this.qrValue);
+    },
+    closeQR: function() {
+      this.isQRShown = false;
+    },
+    encrypt: function(value) {
+      return CryptoJS.AES.encrypt(value, this.secret).toString();
+    },
+    decrypt: function(value) {
+      return CryptoJS.AES.decrypt(value, this.secret).toString(
+        CryptoJS.enc.Utf8
+      );
+    },
+    getHostPort: function() {
+      const url = window.location.href;
+      return url.split("/")[0] + "//" + url.split("/")[2];
+    }
   }
 };
 </script>
